00f3123987d5d0b5d14528df31441f6e3345f9fb
Juan Almeida <juanelojga@gmail.com>
2019-01-20 22:52:07 -0500

    update test case

--- src/components/__tests__/Suggestions.test.js
+++ src/components/__tests__/Suggestions.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, fireEvent, cleanup} from 'react-testing-library'
+import {render, fireEvent, cleanup, getByText as getByTextAssert} from 'react-testing-library'
 
 import * as generators from '../../test/generators'
 import Suggestions from '../Suggestions'
@@ -13,7 +13,7 @@ test('calls onClick with the id of the user clicked', () => {
   const handleClickOutside = jest.fn()
 
   const {getByText} = render(
-    <Suggestions items={users} isVisible={true} onClick={handleClick} onClickOutside={handleClickOutside} />,
+    <Suggestions items={users} isLoading={false} onClick={handleClick} onClickOutside={handleClickOutside} />,
   )
 
   fireEvent.click(getByText(users[0].name))
@@ -24,9 +24,22 @@ test('calls onClick with the id of the user clicked', () => {
   expect(handleClickOutside).not.toHaveBeenCalled()
 })
 
+test('shows no results if items are empty', () => {
+  const users = []
+
+  const handleClick = jest.fn()
+  const handleClickOutside = jest.fn()
+
+  const {container} = render(
+    <Suggestions items={users} isLoading={false} onClick={handleClick} onClickOutside={handleClickOutside} />,
+  )
+
+  getByTextAssert(container, 'No results were found...')
+})
+
 test('snapshot', () => {
   const users = [{id: 1, name: 'Juan Almeida'}, {id: 2, name: 'Patricio Almeida'}]
 
-  const {container} = render(<Suggestions items={users} isVisible={true} />)
+  const {container} = render(<Suggestions items={users} isLoading={false} />)
   expect(container).toMatchSnapshot()
 })
